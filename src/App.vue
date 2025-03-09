abea1b9057f69ca4f59d928c4628dbe92697fa6f
jovan <jovanus@yahoo.co.id>
2025-03-09 22:36:18 +0700

    feat: finishing project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { CheckIcon } from "lucide-vue-next";
+import { CheckIcon, PhoneIcon, MailIcon } from "lucide-vue-next";
 import { RouterView } from "vue-router";
 const currentYear = new Date().getFullYear();
 </script>
 
 <template>
-  <header class="bg-white">
+  <header class="bg-white sticky top-0 z-50">
     <div class="flex items-center justify-between container h-20">
       <a href="#" draggable="false">
         <img
@@ -20,30 +20,31 @@ const currentYear = new Date().getFullYear();
       </a>
 
       <nav>
-        <button
+        <a
+        href="#contact"
           class="rounded-4xl bg-[#1d4762] px-4 h-10 inline-flex items-center text-white relative"
         >
           Get in touch
-        </button>
+      </a>
       </nav>
     </div>
   </header>
 
   <RouterView />
 
-  <footer class="bg-primary text-white">
-    <div class="flex py-12 container gap-6">
+  <footer class="bg-primary text-white" id="contact">
+    <div class="flex flex-col sm:flex-row py-12 container gap-6">
       <div class="flex flex-col basis-3/5 shrink-0 max-w-3xl gap-6">
         <h2 class="text-white font-normal">
           <strong class="text-opacity-100"
-            >It's time to support zero polution,</strong
+            > Connect with Us,</strong
           >
-          <span class="opacity-70"> with renewable resources </span>
+          <span class="opacity-70"> for Tailored Solutions and Expert Guidance</span>
         </h2>
         <div class="flex gap-6">
           <div class="flex items-center gap-2">
             <div
-              class="size-5 flex items-center justify-center rounded-full text-primary bg-white"
+              class="shrink-0 size-5 flex items-center justify-center rounded-full text-primary bg-white"
             >
               <CheckIcon class="size-3.5" />
             </div>
@@ -51,19 +52,32 @@ const currentYear = new Date().getFullYear();
           </div>
           <div class="flex items-center gap-2">
             <div
-              class="size-5 flex items-center justify-center rounded-full text-primary bg-white"
+              class="shrink-0 size-5 flex items-center justify-center rounded-full text-primary bg-white"
             >
               <CheckIcon class="size-3.5" />
             </div>
             Support for the latest technology
           </div>
         </div>
+
+        <div class="flex flex-col gap-2">
+            <a class="flex items-center gap-x-2 text-left" href="tel:02178342780">
+              <PhoneIcon class="size-4 text-white" />
+              <span class="font-medium hover:underline">
+                021-7834-2780 
+                </span>
+            </a>
+            <a
+              class="flex items-center gap-x-2 text-left"
+              href="mailto:inovasi118@gmail.com"
+            >
+              <MailIcon class="size-4 text-white" />
+              <span class="font-medium hover:underline">inovasi118@gmail.com</span>
+            </a>
+          </div>
       </div>
 
       <div class="flex justify-end grow items-center">
-        <!-- <p class="text-white text-balance max-w-md">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consectetur consequatur pariatur maiores atque tempore velit quod, repudiandae aliquam sunt.
-          </p> -->
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8301669419473!2d106.80962160549706!3d-6.290545955850148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1fa5b9f59a3%3A0x772f6f7bc1fbc6c0!2sMenara%20165!5e0!3m2!1sen!2sid!4v1740321602656!5m2!1sen!2sid"
           class="aspect-square w-full border-0 rounded-xl"
@@ -74,7 +88,7 @@ const currentYear = new Date().getFullYear();
     </div>
 
     <div
-      class="flex items-center py-6 text-sm text-slate-200 container border-t border-t-slate-500 max-sm:text-center sm:px-0"
+      class="flex items-center py-6 text-sm text-slate-200 container border-t border-t-slate-500 max-sm:justify-center sm:px-0"
     >
       &copy; {{ currentYear }} PT Inovasi Anugrah Sejahtera. All rights reserved
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,60 @@
 <script setup>
+import { ShieldCheckIcon } from "lucide-vue-next";
+import { ChartNoAxesColumnIncreasing } from "lucide-vue-next";
+import { PenIcon } from "lucide-vue-next";
+import { InfinityIcon } from "lucide-vue-next";
+import { DatabaseIcon } from "lucide-vue-next";
 import { Layers3Icon } from "lucide-vue-next";
+
+
+const softwarePoints = [
+  {
+    icon: Layers3Icon,
+    title: 'Custom-Built Solutions',
+    description: 'Tailored to your business needs for maximum efficiency'
+  },
+  {
+    icon: ShieldCheckIcon,
+    title: 'Advanced Security',
+    description: 'Enterprise-grade protection against cyber threats'
+  },
+  {
+    icon: PenIcon,
+    title: 'Intuitive UX',
+    description: 'User-friendly design for seamless operation'
+  },
+  {
+    icon: ChartNoAxesColumnIncreasing,
+    title: 'Scalable & Future-Ready',
+    description: 'Easily adaptable for business growth'
+  },
+  {
+    icon: InfinityIcon,
+    title: 'Ongoing Support',
+    description: 'Regular updates and dedicated technical assistance'
+  },
+  {
+    icon: DatabaseIcon,
+    title: 'Database Solution',
+    description: 'Our expertise spans database design, optimization, migration, and management'
+  }
+]
 </script>
 
 <template>
   <main>
     <!-- Hero -->
     <section class="md:container">
-      <div class="relative w-full aspect-[2/1]">
-        <div class="absolute inset-0 bg-black/55 rounded-4xl z-10" />
+      <div class="relative w-full h-[500px] sm:h-[550px] max-sm:flex max-sm:flex-col max-sm:items-center">
+        <div class="absolute inset-0 bg-black/55 sm:rounded-4xl z-10" />
         <img
           src="/batik-bg.webp"
           alt="batik background"
-          class="w-full object-cover object-center aspect-[2/1] rounded-4xl absolute inset-0 z-0"
+          class="w-full object-cover object-center h-[500px] sm:h-[550px] sm:rounded-4xl absolute inset-0 z-0"
         />
 
         <div
-          class="flex flex-col w-full max-w-5xl h-full px-16 justify-center z-30 relative"
+          class="flex flex-col w-full max-w-5xl max-sm:pt-24 sm:h-full px-4 sm:px-16 justify-center z-30 relative"
         >
           <p class="tracking-wide text-slate-100">
             #1 General trading company in Indonesia
@@ -26,22 +65,22 @@ import { Layers3Icon } from "lucide-vue-next";
         </div>
 
         <div
-          class="absolute bottom-8 right-16 rounded-4xl bg-white/5 py-4 px-8 backdrop-blur-xs w-full max-w-lg h-1/5 z-30 text-white"
+          class="max-sm:mt-12 sm:absolute sm:bottom-8 sm:right-16 rounded-4xl bg-white/5 py-4 px-8 backdrop-blur-xs w-full max-w-sm sm:max-w-lg h-1/5 z-30 text-white"
         >
           <dl
-            class="grid grid-cols-3 gap-4 [&_dt]:text-4xl [&_dd]:text-sm [&_dd]:opacity-80 [&_dd]:text-balance h-full items-center"
+            class="grid grid-cols-3 gap-4 [&_dt]:text-3xl [&_dt]:sm:text-4xl [&_dd]:text-sm [&_dd]:opacity-80 [&_dd]:text-balance h-full items-start"
           >
             <div class="flex flex-col gap-1.5">
-              <dt>6jt</dt>
-              <dd>The company's annual net income</dd>
+              <dt>10B+</dt>
+              <dd>AUM</dd>
             </div>
             <div class="flex flex-col gap-1.5">
               <dt>500+</dt>
-              <dd>Projects completed worldwide</dd>
+              <dd>Projects completed</dd>
             </div>
             <div class="flex flex-col gap-1.5">
-              <dt>10rb+</dt>
-              <dd>Satisfied customers worldwide</dd>
+              <dt>10K+</dt>
+              <dd>Satisfied customers</dd>
             </div>
           </dl>
         </div>
@@ -49,9 +88,9 @@ import { Layers3Icon } from "lucide-vue-next";
     </section>
 
     <!-- Metal Trading -->
-    <section class="flex flex-col container gap-12 py-16">
-      <div class="flex items-center gap-4 *:flex-1">
-        <h2 class="text-balance text-center">
+    <section class="flex flex-col container gap-4 sm:gap-12 py-8 sm:py-16">
+      <div class="flex sm:flex-row flex-col items-center gap-4 *:flex-1">
+        <h2 class="text-balance sm:text-center">
           Focusing on Quality,
           <span class="text-slate-500 font-normal"
             >we maintain customer trust</span
@@ -67,55 +106,45 @@ import { Layers3Icon } from "lucide-vue-next";
         </p>
       </div>
 
-      <div class="grid grid-cols-5 gap-8 w-full">
-        <div
-          v-for="n in 5"
-          :key="n"
-          class="bg-slate-100 rounded-full p-4 aspect-square flex items-center justify-center"
-        >
-          <img
-            draggable="false"
-            src="/gold-ore.png"
-            alt="metal trading"
-            class="w-1/2 aspect-square object-cover object-center"
-          />
-        </div>
+      <div class="relative h-[500px]">
+        <img src="https://12qvr1x1z8.ufs.sh/f/EqFVW2esDyLuZcyE8BUyAK1blzsP2o9Cm7TGgIwexSMkqBjR" alt="" class="w-full h-full object-cover object-center rounded-4xl">
+        <div class="absolute inset-0 bg-primary/30 rounded-4xl z-10" />
       </div>
     </section>
 
     <!-- Software or AI -->
-    <section class="py-16 bg-slate-100 flex flex-col gap-16 px-4">
-      <h2 class="text-center text-balance max-w-xl mx-auto">
-        <strong class="font-medium">We offer quality,</strong>
+    <section class="py-16 bg-slate-100 flex flex-col gap-8 sm:gap-16 px-4">
+      <h2 class="text-center text-balance mx-auto">
+        <strong class="font-medium">Connect with Us,<br> </strong>
         <span class="text-slate-500 font-normal">
-          with the best materials and service
+          for tailored solutions and expert guidance
         </span>
+        
       </h2>
 
       <dl
-        class="grid grid-cols-3 container px-0 bg-white rounded-4xl shadow-xs overflow-hidden"
+        class="grid grid-cols-1 sm:grid-cols-3 container px-0 bg-white rounded-4xl shadow-xs overflow-hidden"
       >
-        <div v-for="n in 6" class="py-10 px-6 border-r border-b -mr-px -mb-px">
+        <div v-for="{ title, icon, description}, index in softwarePoints" :key="index" class="py-10 px-6 border-r border-b -mr-px -mb-px">
           <div
             class="p-2 shadow rounded-md flex items-center justify-center aspect-square size-10"
           >
-            <Layers3Icon class="text-primary size-5" />
+            <component :is="icon" class="text-primary size-5" />
           </div>
           <dt class="text-2xl tracking-tight mt-6 text-primary font-medium">
-            Layered Security
+            {{title}}
           </dt>
           <dd class="text-slate-600 mt-1.5">
-            with layered security, we ensured the safety of every unit we
-            provided
+            {{description}}
           </dd>
         </div>
       </dl>
     </section>
 
-    <section class="py-12">
+    <section class="py-12 max-sm:px-4">
       <div
-        class="flex rounded-4xl -mt-20 bg-white shadow-sm overflow-hidden z-10 container p-6 gap-6">
-        <img src='https://xnxk060v5p.ufs.sh/f/D0ooyPBt8scP7ZArIBFL2b4tR1G3W6FmzliNYhXBjDQyK09o' alt="property" class=" object-cover w-1/2 aspect-square rounded-xl" />
+        class="flex flex-col sm:flex-row rounded-4xl sm:-mt-20 -mt-24 bg-white shadow-sm overflow-hidden z-10 container p-6 gap-6">
+        <img src='https://xnxk060v5p.ufs.sh/f/D0ooyPBt8scP7ZArIBFL2b4tR1G3W6FmzliNYhXBjDQyK09o' alt="property" class="object-cover sm:w-1/2 aspect-square rounded-xl" />
 
         <div class="flex flex-col gap-6 text-balance">
           <h2 class="text-4xl ">A Trusted Advisor for Strategic Real Estate Investments</h2>
