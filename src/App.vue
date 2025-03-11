16d1d21a2c3084738daad8e907796cf811866fd1
jovan <jovanus@yahoo.co.id>
2025-03-12 00:03:56 +0700

    feat: adjust software copy and new logo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { CheckIcon, PhoneIcon, MailIcon } from "lucide-vue-next";
-import { RouterView } from "vue-router";
-const currentYear = new Date().getFullYear();
+import { CheckIcon, PhoneIcon, MailIcon } from 'lucide-vue-next'
+import { RouterView } from 'vue-router'
+const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <header class="bg-white sticky top-0 z-50">
-    <div class="flex items-center justify-between container h-20">
-      <a href="#" draggable="false">
+  <header class="sticky top-0 z-50 bg-white">
+    <div class="container flex h-20 items-center justify-between">
+      <a href="#" draggable="false" class="flex items-center">
         <img
           src="https://12qvr1x1z8.ufs.sh/f/EqFVW2esDyLuX4F4nFyjYV6kKIUpeHhDbnd1RzO925Jxqi0g"
           alt="trademark logo"
@@ -17,15 +17,24 @@ const currentYear = new Date().getFullYear();
           decoding="async"
           draggable="false"
         />
+        <img
+          src="https://xnxk060v5p.ufs.sh/f/D0ooyPBt8scPwL7ZN0QFEsJcpuf8glRynTvrCUaOLM4XzhBK"
+          alt="inovasi wordmark"
+          class="-ml-3 h-20"
+          loading="eager"
+          fetchpriority="high"
+          decoding="async"
+          draggable="false"
+        />
       </a>
 
       <nav>
         <a
-        href="#contact"
-          class="rounded-4xl bg-[#1d4762] px-4 h-10 inline-flex items-center text-white relative"
+          href="#contact"
+          class="relative inline-flex h-10 items-center rounded-4xl bg-[#1d4762] px-4 text-white"
         >
           Get in touch
-      </a>
+        </a>
       </nav>
     </div>
   </header>
@@ -33,18 +42,16 @@ const currentYear = new Date().getFullYear();
   <RouterView />
 
   <footer class="bg-primary text-white" id="contact">
-    <div class="flex flex-col sm:flex-row py-12 container gap-6">
-      <div class="flex flex-col basis-3/5 shrink-0 max-w-3xl gap-6">
-        <h2 class="text-white font-normal">
-          <strong class="text-opacity-100"
-            > Connect with Us,</strong
-          >
+    <div class="container flex flex-col gap-6 py-12 sm:flex-row">
+      <div class="flex max-w-3xl shrink-0 basis-3/5 flex-col gap-6">
+        <h2 class="font-normal text-white">
+          <strong class="text-opacity-100">Connect with Us,</strong>
           <span class="opacity-70"> for Tailored Solutions and Expert Guidance</span>
         </h2>
         <div class="flex gap-6">
           <div class="flex items-center gap-2">
             <div
-              class="shrink-0 size-5 flex items-center justify-center rounded-full text-primary bg-white"
+              class="text-primary flex size-5 shrink-0 items-center justify-center rounded-full bg-white"
             >
               <CheckIcon class="size-3.5" />
             </div>
@@ -52,7 +59,7 @@ const currentYear = new Date().getFullYear();
           </div>
           <div class="flex items-center gap-2">
             <div
-              class="shrink-0 size-5 flex items-center justify-center rounded-full text-primary bg-white"
+              class="text-primary flex size-5 shrink-0 items-center justify-center rounded-full bg-white"
             >
               <CheckIcon class="size-3.5" />
             </div>
@@ -61,26 +68,21 @@ const currentYear = new Date().getFullYear();
         </div>
 
         <div class="flex flex-col gap-2">
-            <a class="flex items-center gap-x-2 text-left" href="tel:02178342780">
-              <PhoneIcon class="size-4 text-white" />
-              <span class="font-medium hover:underline">
-                021-7834-2780 
-                </span>
-            </a>
-            <a
-              class="flex items-center gap-x-2 text-left"
-              href="mailto:inovasi118@gmail.com"
-            >
-              <MailIcon class="size-4 text-white" />
-              <span class="font-medium hover:underline">inovasi118@gmail.com</span>
-            </a>
-          </div>
+          <a class="flex items-center gap-x-2 text-left" href="tel:02178342780">
+            <PhoneIcon class="size-4 text-white" />
+            <span class="font-medium hover:underline"> 021-7834-2780 </span>
+          </a>
+          <a class="flex items-center gap-x-2 text-left" href="mailto:inovasi118@gmail.com">
+            <MailIcon class="size-4 text-white" />
+            <span class="font-medium hover:underline">inovasi118@gmail.com</span>
+          </a>
+        </div>
       </div>
 
-      <div class="flex justify-end grow items-center">
+      <div class="flex grow items-center justify-end">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8301669419473!2d106.80962160549706!3d-6.290545955850148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1fa5b9f59a3%3A0x772f6f7bc1fbc6c0!2sMenara%20165!5e0!3m2!1sen!2sid!4v1740321602656!5m2!1sen!2sid"
-          class="aspect-square w-full border-0 rounded-xl"
+          class="aspect-square w-full rounded-xl border-0"
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
@@ -88,7 +90,7 @@ const currentYear = new Date().getFullYear();
     </div>
 
     <div
-      class="flex items-center py-6 text-sm text-slate-200 container border-t border-t-slate-500 max-sm:justify-center sm:px-0"
+      class="container flex items-center border-t border-t-slate-500 py-6 text-sm text-slate-200 max-sm:justify-center sm:px-0"
     >
       &copy; {{ currentYear }} PT Inovasi Anugrah Sejahtera. All rights reserved
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { ShieldCheckIcon } from "lucide-vue-next";
-import { ChartNoAxesColumnIncreasing } from "lucide-vue-next";
-import { PenIcon } from "lucide-vue-next";
-import { InfinityIcon } from "lucide-vue-next";
-import { DatabaseIcon } from "lucide-vue-next";
-import { Layers3Icon } from "lucide-vue-next";
-
+import { ShieldCheckIcon } from 'lucide-vue-next'
+import { ChartNoAxesColumnIncreasing } from 'lucide-vue-next'
+import { PenIcon } from 'lucide-vue-next'
+import { InfinityIcon } from 'lucide-vue-next'
+import { DatabaseIcon } from 'lucide-vue-next'
+import { Layers3Icon } from 'lucide-vue-next'
 
 const softwarePoints = [
   {
@@ -45,30 +44,28 @@ const softwarePoints = [
   <main>
     <!-- Hero -->
     <section class="md:container">
-      <div class="relative w-full h-[500px] sm:h-[550px] max-sm:flex max-sm:flex-col max-sm:items-center">
-        <div class="absolute inset-0 bg-black/55 sm:rounded-4xl z-10" />
+      <div
+        class="relative h-[500px] w-full max-sm:flex max-sm:flex-col max-sm:items-center sm:h-[550px]"
+      >
+        <div class="absolute inset-0 z-10 bg-black/55 sm:rounded-4xl" />
         <img
           src="/batik-bg.webp"
           alt="batik background"
-          class="w-full object-cover object-center h-[500px] sm:h-[550px] sm:rounded-4xl absolute inset-0 z-0"
+          class="absolute inset-0 z-0 h-[500px] w-full object-cover object-center sm:h-[550px] sm:rounded-4xl"
         />
 
         <div
-          class="flex flex-col w-full max-w-5xl max-sm:pt-24 sm:h-full px-4 sm:px-16 justify-center z-30 relative"
+          class="relative z-30 flex w-full max-w-5xl flex-col justify-center px-4 max-sm:pt-24 sm:h-full sm:px-16"
         >
-          <p class="tracking-wide text-slate-100">
-            #1 General trading company in Indonesia
-          </p>
-          <h1 class="text-balance text-white">
-            Delivering Excellence Across Industries
-          </h1>
+          <p class="tracking-wide text-slate-100">#1 General trading company in Indonesia</p>
+          <h1 class="text-balance text-white">Delivering Excellence Across Industries</h1>
         </div>
 
         <div
-          class="max-sm:mt-12 sm:absolute sm:bottom-8 sm:right-16 rounded-4xl bg-white/5 py-4 px-8 backdrop-blur-xs w-full max-w-sm sm:max-w-lg h-1/5 z-30 text-white"
+          class="z-30 h-1/5 w-full max-w-sm rounded-4xl bg-white/5 px-8 py-4 text-white backdrop-blur-xs max-sm:mt-12 sm:absolute sm:right-16 sm:bottom-8 sm:max-w-lg"
         >
           <dl
-            class="grid grid-cols-3 gap-4 [&_dt]:text-3xl [&_dt]:sm:text-4xl [&_dd]:text-sm [&_dd]:opacity-80 [&_dd]:text-balance h-full items-start"
+            class="grid h-full grid-cols-3 items-start gap-4 [&_dd]:text-sm [&_dd]:text-balance [&_dd]:opacity-80 [&_dt]:text-3xl [&_dt]:sm:text-4xl"
           >
             <div class="flex flex-col gap-1.5">
               <dt>10B+</dt>
@@ -88,54 +85,61 @@ const softwarePoints = [
     </section>
 
     <!-- Metal Trading -->
-    <section class="flex flex-col container gap-4 sm:gap-12 py-8 sm:py-16">
-      <div class="flex sm:flex-row flex-col items-center gap-4 *:flex-1">
+    <section class="container flex flex-col gap-4 py-8 sm:gap-12 sm:py-16">
+      <div class="flex flex-col items-center gap-4 *:flex-1 sm:flex-row">
         <h2 class="text-balance sm:text-center">
           Focusing on Quality,
-          <span class="text-slate-500 font-normal"
-            >we maintain customer trust</span
-          >
+          <span class="font-normal text-slate-500">we maintain customer trust</span>
         </h2>
         <p>
-          PT Inovasi Anugerah Sejahtera menyediakan layanan investasi logam
-          mulia yang dapat menjadi alternatif investasi yang aman ditengah pasar
-          yang fluktuatif. Dengan sistem transaksi yang transparan dan harga
-          yang kompetitif, perusahaan ini memberikan kemudahan bagi para
-          investor yang ingin menambah portofolio investasi mereka dalam bentuk
-          logam mulia dengan nilai jangka panjang.
+          PT Inovasi Anugerah Sejahtera menyediakan layanan investasi logam mulia yang dapat menjadi
+          alternatif investasi yang aman ditengah pasar yang fluktuatif. Dengan sistem transaksi
+          yang transparan dan harga yang kompetitif, perusahaan ini memberikan kemudahan bagi para
+          investor yang ingin menambah portofolio investasi mereka dalam bentuk logam mulia dengan
+          nilai jangka panjang.
         </p>
       </div>
 
       <div class="relative h-[500px]">
-        <img src="https://12qvr1x1z8.ufs.sh/f/EqFVW2esDyLuZcyE8BUyAK1blzsP2o9Cm7TGgIwexSMkqBjR" alt="" class="w-full h-full object-cover object-center rounded-4xl">
-        <div class="absolute inset-0 bg-primary/30 rounded-4xl z-10" />
+        <img
+          src="https://12qvr1x1z8.ufs.sh/f/EqFVW2esDyLuZcyE8BUyAK1blzsP2o9Cm7TGgIwexSMkqBjR"
+          alt=""
+          class="h-full w-full rounded-4xl object-cover object-center"
+        />
+        <div class="bg-primary/30 absolute inset-0 z-10 rounded-4xl" />
       </div>
     </section>
 
     <!-- Software or AI -->
-    <section class="py-16 bg-slate-100 flex flex-col gap-8 sm:gap-16 px-4">
-      <h2 class="text-center text-balance mx-auto">
-        <strong class="font-medium">Connect with Us,<br> </strong>
-        <span class="text-slate-500 font-normal">
-          for tailored solutions and expert guidance
-        </span>
-        
-      </h2>
+    <section class="flex flex-col gap-8 bg-slate-100 px-4 py-16 sm:gap-16">
+      <div class="flex flex-col">
+        <h2 class="mx-auto text-center text-balance">
+          <strong class="font-medium">Innovative Consulting,<br /> </strong>
+          <span class="font-normal text-slate-500"> for Scalable and Secure Software </span>
+        </h2>
+        <p class="text-center text-balance">
+          menghadirkan solusi piranti lunak berkualitas untuk sektor komersial, industri,
+          manufaktur, dan distribusi. Didukung tenaga ahli berpengalaman, kami merancang sistem yang
+          efektif, efisien, dan tepat guna guna mendukung operasional bisnis di era digital.
+        </p>
+      </div>
 
       <dl
-        class="grid grid-cols-1 sm:grid-cols-3 container px-0 bg-white rounded-4xl shadow-xs overflow-hidden"
+        class="container grid grid-cols-1 overflow-hidden rounded-4xl bg-white px-0 shadow-xs sm:grid-cols-3"
       >
-        <div v-for="{ title, icon, description}, index in softwarePoints" :key="index" class="py-10 px-6 border-r border-b -mr-px -mb-px">
-          <div
-            class="p-2 shadow rounded-md flex items-center justify-center aspect-square size-10"
-          >
+        <div
+          v-for="({ title, icon, description }, index) in softwarePoints"
+          :key="index"
+          class="-mr-px -mb-px border-r border-b px-6 py-10"
+        >
+          <div class="flex aspect-square size-10 items-center justify-center rounded-md p-2 shadow">
             <component :is="icon" class="text-primary size-5" />
           </div>
-          <dt class="text-2xl tracking-tight mt-6 text-primary font-medium">
-            {{title}}
+          <dt class="text-primary mt-6 text-2xl font-medium tracking-tight">
+            {{ title }}
           </dt>
-          <dd class="text-slate-600 mt-1.5">
-            {{description}}
+          <dd class="mt-1.5 text-slate-600">
+            {{ description }}
           </dd>
         </div>
       </dl>
@@ -143,13 +147,24 @@ const softwarePoints = [
 
     <section class="py-12 max-sm:px-4">
       <div
-        class="flex flex-col sm:flex-row rounded-4xl sm:-mt-20 -mt-24 bg-white shadow-sm overflow-hidden z-10 container p-6 gap-6">
-        <img src='https://xnxk060v5p.ufs.sh/f/D0ooyPBt8scP7ZArIBFL2b4tR1G3W6FmzliNYhXBjDQyK09o' alt="property" class="object-cover sm:w-1/2 aspect-square rounded-xl" />
+        class="z-10 container -mt-24 flex flex-col gap-6 overflow-hidden rounded-4xl bg-white p-6 shadow-sm sm:-mt-20 sm:flex-row"
+      >
+        <img
+          src="https://xnxk060v5p.ufs.sh/f/D0ooyPBt8scP7ZArIBFL2b4tR1G3W6FmzliNYhXBjDQyK09o"
+          alt="property"
+          class="aspect-square rounded-xl object-cover sm:w-1/2"
+        />
 
         <div class="flex flex-col gap-6 text-balance">
-          <h2 class="text-4xl ">A Trusted Advisor for Strategic Real Estate Investments</h2>
+          <h2>A Trusted Advisor for Strategic Real Estate Investments</h2>
           <p>
-            Di sektor properti, PT Inovasi Anugerah Sejahtera menawarkan layanan pembelian, penjualan dan penyewaan properti komersial dan industri, seperti gedung perkantoran, hotel & apartemen, fasilitas pabrik, serta ruang gudang. Perusahaan ini memahami pentingnya lokasi yang strategis, harga yang kompetitif dan kualitas properti yang terbaik guna mendukung kelancaran operasional bisnis. Dengan berbagai pilihan properti yang dapat disesuaikan dengan kebutuhan usaha, PT Inovasi Anugerah Sejahtera memastikan setiap klien mendapatkan solusi properti yang terbaik, tepat dan efisien.
+            Di sektor properti, PT Inovasi Anugerah Sejahtera menawarkan layanan pembelian,
+            penjualan dan penyewaan properti komersial dan industri, seperti gedung perkantoran,
+            hotel & apartemen, fasilitas pabrik, serta ruang gudang. Perusahaan ini memahami
+            pentingnya lokasi yang strategis, harga yang kompetitif dan kualitas properti yang
+            terbaik guna mendukung kelancaran operasional bisnis. Dengan berbagai pilihan properti
+            yang dapat disesuaikan dengan kebutuhan usaha, PT Inovasi Anugerah Sejahtera memastikan
+            setiap klien mendapatkan solusi properti yang terbaik, tepat dan efisien.
           </p>
         </div>
       </div>
